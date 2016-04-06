677cbb49bc579fc68e3836cfd5ec25a701a423d3
Leon Gabriel Kyagami <leonkyagami@gmail.com>
2016-04-07 07:56:50 +0800

    Update Basic Import layout

--- assets/js/button.js
+++ assets/js/button.js
@@ -8,11 +8,15 @@ function addRow() {
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(5);
     cell1.innerHTML = x;
     cell2.innerHTML = "<input type=\"text\" name=\"mahang\" class=\"form-control\">";
     cell3.innerHTML = "<input type=\"text\" name=\"tenhang\" class=\"form-control\">";
     cell4.innerHTML = "<input type=\"text\" name=\"soluong\" class=\"form-control\">";
-    cell5.innerHTML = "<input type=\"text\" name=\"gianhap\" class=\"form-control\">";
+    cell5.innerHTML = "<input  type=\"text\" name=\"gianhap\" class=\"form-control\">";
+    cell6.innerHTML = "<input type=\"text\" name=\"soluong\" class=\"form-control\">";
+    cell7.innerHTML = "<input type=\"text\" name=\"soluong\" class=\"form-control\">";
 }
 
 
